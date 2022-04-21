3d7fa37ba15ff5f6bfa1543c5c6a04591f945729
gmantuhac <103173107+gmantuhac@users.noreply.github.com>
2022-04-21 10:45:35 -0700

    Used Command to Run
    javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,10 @@
-import static org.junit.Assert.*;
-import org.junit.*;
-public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+import static org.junit.Assert.*; // Imports a package needed for JUnit
+import org.junit.*; // Imports another package needed for JUnit
+
+public class MarkdownParseTest { // The class itself
+    @Test // Tells program that this is a test
+    public void addition() { // Method for the test
+        assertEquals(2, 1 + 1); // Compares the expected vs output
     }
 }
 
