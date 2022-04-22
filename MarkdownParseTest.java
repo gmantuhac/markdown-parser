d7e3b56c85622aa356a84781f597ec30a9dd01bc
gmantuhac <103173107+gmantuhac@users.noreply.github.com>
2022-04-22 13:19:42 -0700

    End of Lab 4 additions

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,24 @@
 import static org.junit.Assert.*; // Imports a package needed for JUnit
 import org.junit.*; // Imports another package needed for JUnit
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MarkdownParseTest { // The class itself
     @Test // Tells program that this is a test
     public void addition() { // Method for the test
         assertEquals(2, 1 + 1); // Compares the expected vs output
     }
+    
+    @Test
+    public void testGetLinks() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(List.of("https://something.com", "some-thing.html"), links);
+    }
 }
+
 
--- test-fileinmiddle.md
+++ test-fileinmiddle.md
@@ -1,4 +1,5 @@
 
+[
 
 [Link](ucsd.edu)
 
